3501f571e484300ea4d0bc4f135af466cd954bad
DxDTitan <728472@student.dusd.net>
2025-05-22 09:56:27 -0700

    clickity clackity

--- ClickyClackyU5P2Shiu/Assets/Scripts/GameManager.cs
+++ ClickyClackyU5P2Shiu/Assets/Scripts/GameManager.cs
@@ -7,15 +7,21 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score:" + score;    }
+        UpdatedScore(0);
+
+      
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,14 +30,25 @@ public class GameManager : MonoBehaviour
     }
  IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
-
 
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+   public void UpdatedScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 }
 
